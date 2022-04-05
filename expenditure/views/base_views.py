5e27e74ddfc858e85e407c8d1dcd2ddfde933dd4
yiryeong0720 <yiryeong0720@gmail.com>
2022-04-05 14:30:46 +0900

    1. add paging feature 2. add sorting feature
    Signed-off-by: yiryeong0720 <yiryeong0720@gmail.com>

--- expenditure/views/base_views.py
+++ expenditure/views/base_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, redirect
 from rest_framework.decorators import permission_classes
@@ -20,7 +21,8 @@ def expenditure(request):
         category_list = Category.objects.filter(uid=user)
         today = date.today().strftime("%Y-%m-%d")
 
-        # page = request.GET.get('page', '1')
+        page = request.GET.get('page', '1')
+        so = request.GET.get('so', 'recent')
         search_date = request.GET.get('search_date', today)
         kw = request.GET.get('kw', '')
         selected_category = request.GET.get('selected_category', '0')
@@ -29,7 +31,10 @@ def expenditure(request):
         end_date = datetime.strptime(search_date, "%Y-%m-%d")
         start_date = end_date - timedelta(days=date_scope)
 
-        expenditure_list = Expenditure.objects.filter(uid=user, pay_date__range=[start_date, search_date])
+        if so == 'recent':
+            expenditure_list = Expenditure.objects.filter(uid=user, pay_date__range=[start_date, search_date]).order_by('-pay_date')
+        else:
+            expenditure_list = Expenditure.objects.filter(uid=user, pay_date__range=[start_date, search_date]).order_by('pay_date')
 
         if selected_category != '0':
             expenditure_list = expenditure_list.filter(
@@ -49,9 +54,10 @@ def expenditure(request):
         for data in serializer.data:
             data['category'] = category_list.get(id=data['cid']).name
 
-        # # paging
-        # paginator = Paginator(expenditure_list, 10)  # 페이지당 10개씩 보여 주기
-        # page_obj = paginator.get_page(page)
+        # paging
+        paginator = Paginator(serializer.data, 5)  # 페이지당 5개씩 보여 주기
+        page_obj = paginator.get_page(page)
 
-        context = {'expenditure_list': serializer.data, 'category_list': category_list, "selected_category": int(selected_category), "search_date": search_date, "date_scope": date_scope, "kw": kw}
+        context = {'expenditure_list': page_obj, 'category_list': category_list, "selected_category": int(selected_category),
+                   "search_date": search_date, "date_scope": date_scope, "kw": kw, "so": so}
         return render(request, 'expenditure/list.html', context)
